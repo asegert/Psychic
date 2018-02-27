ed9b33350a2d78fe1a3483a1a29b118a87af00be
asegert <zannagal@live.ca>
2018-02-27 10:18:30 -0800

    Shuffle Commit
    Added shuffle function and capabilities to shuffle the crystal balls. Also have the card being placed under one and being reproduced once the shuffle is finished. Also have temporary clicking functions for the crystal balls.

--- js/states/Game.js
+++ js/states/Game.js
@@ -31,12 +31,74 @@ Psychic.GameState = {
                 let lastCardTween = this.add.tween(this.card).to({x: this.card.x - 20, y: 540}, 1000, "Linear", true);
                 lastCardTween.onComplete.add(function()
                 {
-                    this.add.tween(this.crystalBall[num]).to({rotation: 0}, 1000, "Linear", true);
+                    this.world.bringToTop(this.crystalBall[0]);
+                    this.world.bringToTop(this.crystalBall[1]);
+                    this.world.bringToTop(this.crystalBall[2]);
+                    let drop = this.add.tween(this.crystalBall[num]).to({rotation: 0}, 1000, "Linear", true);
+                    drop.onComplete.add(function()
+                    {
+                        this.card.destroy();
+                        this.clickable(num);
+                        this.shuffle(0, 10, 1000, num);//min 300
+                    }, this);
                 }, this);
-                //Oncomplete rotate back the ball
             }, this);
         }, this);
-        //call new func for shuffle
+    },
+    clickable: function(index)
+    {
+        for(let i = 0, len = this.crystalBall.length; i<len; i++)
+        {
+            if(i == index)
+            {
+                this.crystalBall[i].inputEnabled = true;
+                this.crystalBall[i].events.onInputDown.add(function()
+                {
+                    console.log('You got the right ball');
+                }, this);
+            }
+            else
+            {
+                this.crystalBall[i].inputEnabled = true;
+                this.crystalBall[i].events.onInputDown.add(function()
+                {
+                    console.log('You got the wrong ball');
+                }, this);
+            }
+        }
+    },
+    shuffle: function(index, max, speed, num)
+    {
+        let rand = Math.floor(Math.random() * 3);
+        let rand2 = Math.floor(Math.random() * 3);
+
+        while(rand2 === rand)
+        {
+            rand2 = Math.floor(Math.random() * 3);
+        }
+            
+        let oldx = this.crystalBall[rand].x;
+            
+        this.add.tween(this.crystalBall[rand]).to({x: this.crystalBall[rand2].x}, speed, "Linear", true);
+        let switchTween = this.add.tween(this.crystalBall[rand2]).to({x: oldx}, speed, "Linear", true);
+        switchTween.onComplete.add(function()
+        {
+            if(index<max)
+            {
+                this.shuffle((index + 1), max, speed, num);
+            }
+            else
+            {
+                this.replaceCard(num);
+            }
+        }, this);
+        
+    },
+    replaceCard: function(index)
+    {
+        this.card = this.add.sprite(this.crystalBall[index].x + 70, 520, 'cardBack');
+        this.card.scale.setTo(0.5, 0.1);
+        this.world.bringToTop(this.crystalBall[index]);    
     },
     update: function ()
     {
